4460b61e77a555addca50f838ac3d9e5edd9db19
Maciej Małecki <maciej.malecki@hotmail.com>
2011-03-30 15:13:10 +0200

    Changed tabs to spaces. *Basic* functionality available.

--- netwiseyum/config.py
+++ netwiseyum/config.py
@@ -1,22 +1,28 @@
 from argparse import ArgumentParser
 
 class Config(object):
-	def __init__(self, data={ }):
-		self.data = { }
+    def __init__(self, data={ }):
+        self.data = { }
 		
-	def read_all(self):
-		self.read_args()
+    def read_all(self):
+        self.read_args()
+    
+    def __getitem__(self, item):
+        return self.data[item]
 	
-	def __getitem__(self, item):
-		return self.data[item]
-	
-	def read_args(self):
-		parser = ArgumentParser(description='Run netwise-yum server.')
-		parser.add_argument('-p', 
-	                    	help='Port which server will run on.',
-	                    	dest='port',
-	                    	type=int,
-	                    	default=80)             
-		args = parser.parse_args().__dict__
-		for (k, v) in args.iteritems():
-			self.data[k] = v
+    def read_args(self):
+        parser = ArgumentParser(description='Run netwise-yum server.')
+        parser.add_argument('-p', 
+                            help='Port which server will run on.',
+                            dest='port',
+                            type=int,
+                            default=80)
+        parser.add_argument('-c',
+                            help='Cache directory.',
+                            dest='cache',
+                            default='cache')
+        parser.add_argument('-s',
+                            help='Original repository server.',
+                            dest='server',
+                            default='http://download.fedoraproject.org/')
+        self.data = parser.parse_args().__dict__
--- netwiseyum/server.py
+++ netwiseyum/server.py
@@ -1,21 +1,59 @@
 from config import Config
+from urlparse import urlparse
+import urllib2
 import tornado.web
 import tornado.ioloop
+import os
 
 class Server(object):
-	def __init__(self, config=None):
-		self.config = config
-		if self.config is None:
-			self.config = Config()
-			self.config.read_all()
+    def __init__(self, config=None):
+        self.config = config
+        if self.config is None:
+            self.config = Config()
+            self.config.read_all()
 	
-	def run(self):
-		application = tornado.web.Application([
-			(r"/(.+)", Handler)
-		])
-		application.listen(self.config['port'])
-		tornado.ioloop.IOLoop.instance().start()
+    def run(self):
+        Handler.config = self.config
+        application = tornado.web.Application([
+            (r"/(.+)", Handler)
+        ])
+        application.listen(self.config['port'])
+        tornado.ioloop.IOLoop.instance().start()
 
-class Handler(object):
-	def get(self, path):
-		pass
+class Handler(tornado.web.RequestHandler):
+    config = None
+    BLOCK_SIZE = 8192
+    
+    def __init__(self, *args, **kwargs):
+        tornado.web.RequestHandler.__init__(self, *args, **kwargs)
+        self.server = urlparse(self.config['server'])
+    
+    def is_cached(self, filename):
+        return os.path.exists(self.make_cache_path(filename))
+    
+    def make_cache_path(self, filename):
+        return os.path.join(self.config['cache'], self.config['server'], filename)
+    
+    def save_cache(self, filename, content):
+        path = self.make_cache_path(filename)
+        directory = os.path.dirname(path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(path, "w") as f:
+            f.write(content)        
+        
+    def get(self, path):
+        print path
+        print "Starting download of " + self.config['server'] + '/' + path
+        
+        resp = urllib2.urlopen(self.config['server'] + '/' + path)
+        content = ""
+        while True:
+            buf = resp.read(self.BLOCK_SIZE)
+            if not buf:
+                break
+            content += buf
+            self.write(buf)
+        
+        print "Downloaded."
+        self.save_cache(path, content)
